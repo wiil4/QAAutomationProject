00e8ea23bee432015928dc0d961f525918524944
Wilfor Ibanez <aln.wil4@outlook.es>
2023-05-22 23:33:31 -0400

    Yopmail test files added

---
+++ YopmailAndTodoist/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace YopmailAndTodoist
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ YopmailAndTodoist/src/code/control/Button.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YopmailAndTodoist.src.code.control
+{
+    public class Button : Control
+    {
+        public Button(By locator) : base(locator)
+        {
+        }
+
+        public bool IsEnabled()
+        {
+            FindControl();
+            return control.Enabled;
+        }
+    }
+}
